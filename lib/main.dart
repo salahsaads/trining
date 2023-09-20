488537019d4a0fc97f2f22d1f544a0246efbfcca
Salah_Saad <kassabksab@gmail.com>
2023-09-20 23:13:34 +0300

    start

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Salah'),
+              backgroundColor: Colors.amber,
+            ),
+            body:Image.asset('image/salah.jpg')));
+  }
+}
